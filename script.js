a6d53b7079658d4633d364624f3e031a965f1e87
zzamisj <zzamisj@gmail.com>
2023-05-29 09:16:01 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const nameElement = document.getElementById("name");
+const introElement = document.getElementById("intro");
+
+if (nameElement && introElement) {
+  nameElement.textContent = "Jomer Amistoso";
+  introElement.textContent = "QA Tester";
+}
